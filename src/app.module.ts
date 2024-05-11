742a6f8477d898c0b68e8dcfa84eaa02e64f04c8
dongsipan <dongsipan@126.com>
2024-05-11 11:38:35 +0800

    解决swagger 路径问题

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PoetryModule } from './poetry/poetry.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [PoetryModule, PrismaModule],
 })
 export class AppModule {}
